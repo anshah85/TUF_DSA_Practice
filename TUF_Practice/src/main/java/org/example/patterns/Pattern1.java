1cca4783ee3ac0ee0996d2d6287c29ac8f818396
Akshat Shah <ashah85@binghamton.edu>
2024-06-17 20:09:04 -0400

    Pattern1

---
+++ TUF_Practice/src/main/java/org/example/patterns/Pattern1.java
@@ -0,0 +1,22 @@
+/* *****************************************************************************
+ *  Name:              Ada Lovelace
+ *  Coursera User ID:  123456
+ *  Last modified:     October 16, 1842
+ **************************************************************************** */
+
+package org.example.patterns;
+
+public class Pattern1 {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+
+        // Print a pattern
+
+        for (int i = 0; i < 4; i++) {
+            for (int j = 0; j < 4; j++) {
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+    }
+}
